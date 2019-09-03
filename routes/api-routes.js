b5e86bba890ddce5ff94a75d4ffc9f554e7bbe7b
michaelmccarty <mccartyatl@gmail.com>
2019-09-03 11:42:11 -0400

    Added Google login strategy.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,9 @@
 const db = require('../models');
 const path = require("path");
-const passport = require('passport');
+//const passport = require('passport');
 
 
-module.exports = function(app) {
+module.exports = function (app, passport) {
 
   // app.post('/api/login', function(req, res){
   //   const userAccountName = req.body.username;
@@ -32,38 +32,57 @@ module.exports = function(app) {
 
 
   app.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/infobox',
-    failureRedirect: '/login',
-    failerFlash:true
-  })
-);
+    passport.authenticate('local', {
+      successRedirect: '/infobox',
+      failureRedirect: '/login',
+      failerFlash: true
+    })
+  );
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/logIn.html'))
-  // if (req.user) {
-  //   res.send(`Welcome ${req.user.displayName}!`);
-  // } else res.redirect('/login');
-});
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/logIn.html'))
+    // if (req.user) {
+    //   res.send(`Welcome ${req.user.displayName}!`);
+    // } else res.redirect('/login');
+  });
+
+  // GET /auth/google
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  The first step in G oogle authentication will involve
+  //   redirecting the user to google.com.  After authorization, Google
+  //   will redirect the user back to this application at /auth/google/callback
+  app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+  // GET /auth/google/callback
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  If authentication fails, the user will be redirected back to the
+  //   login page.  Otherwise, the primary route function function will be called,
+  //   which, in this example, will redirect the user to the home page.
+  app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+      res.redirect('/');
+    });
 
 
-  app.post('/api/newAccounts', function(req, res) {
+  app.post('/api/newAccounts', function (req, res) {
     const newAcctInfo = req.body;
-    db.account.create(req.body).then(function(dbPost) {
+    db.account.create(req.body).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
   // delete?
-  app.get('/auth/isauth', function(req, res) {
+  app.get('/auth/isauth', function (req, res) {
     console.log(req.user);
-    if ( req.user ) return res.send({success: 1})
+    if (req.user) return res.send({ success: 1 })
 
-    res.status(401).send({message: 'Not Authorized'});
+    res.status(401).send({ message: 'Not Authorized' });
   })
 
 
-  function getThing(userAccountName){
+  function getThing(userAccountName) {
 
   }
 
